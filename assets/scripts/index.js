f15c221e6259263d258a7e182b33dd5b931f9437
gwynlordofcinder <pabloandrestejedor@icloud.com>
2021-06-19 04:02:52 -0300

    pwa implementation

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,41 +1,47 @@
-const input = document.getElementById("input");
-const d4 = document.getElementById("buttond4");
-const d6 = document.getElementById("buttond6");
-const d8 = document.getElementById("buttond8");
-const d10 = document.getElementById("buttond10");
-const d12 = document.getElementById("buttond12");
-const d20 = document.getElementById("buttond20");
+const input = document.getElementById('input');
+const d4 = document.getElementById('buttond4');
+const d6 = document.getElementById('buttond6');
+const d8 = document.getElementById('buttond8');
+const d10 = document.getElementById('buttond10');
+const d12 = document.getElementById('buttond12');
+const d20 = document.getElementById('buttond20');
+const showLog = document.getElementById('showLog');
 let log = [];
-const showLog = document.getElementById("showLog");
 
-const getRandomNumber = (diceSelector) =>
+const getRandomNumber = diceSelector =>
   Math.floor(Math.random() * diceSelector + 1);
 
-const writeToLog = (result) => log.push(result);
+const writeToLog = result => log.push(result);
 
-d4.addEventListener("click", () => {
-  input.value = "d4" + " " + getRandomNumber(4);
+d4.addEventListener('click', () => {
+  input.value = 'd4' + ' ' + getRandomNumber(4);
   writeToLog(input.value);
 });
-d6.addEventListener("click", () => {
-  input.value = "d6" + " " + getRandomNumber(6);
+d6.addEventListener('click', () => {
+  input.value = 'd6' + ' ' + getRandomNumber(6);
   writeToLog(input.value);
 });
-d8.addEventListener("click", () => {
-  input.value = "d8" + " " + getRandomNumber(8);
+d8.addEventListener('click', () => {
+  input.value = 'd8' + ' ' + getRandomNumber(8);
   writeToLog(input.value);
 });
-d10.addEventListener("click", () => {
-  input.value = "d10" + " " + getRandomNumber(10);
+d10.addEventListener('click', () => {
+  input.value = 'd10' + ' ' + getRandomNumber(10);
   writeToLog(input.value);
 });
-d12.addEventListener("click", () => {
-  input.value = "d12" + " " + getRandomNumber(12);
+d12.addEventListener('click', () => {
+  input.value = 'd12' + ' ' + getRandomNumber(12);
   writeToLog(input.value);
 });
-d20.addEventListener("click", () => {
-  input.value = "d20" + " " + getRandomNumber(20);
+d20.addEventListener('click', () => {
+  input.value = 'd20' + ' ' + getRandomNumber(20);
   writeToLog(input.value);
 });
 
-showLog.addEventListener("click", () => alert(log));
+// showLog.addEventListener("click", () => alert(log));
+
+showLog.addEventListener('click', () =>
+  log.length > 0
+    ? alert(log)
+    : alert("Maybe you should roll a dice first don't you?")
+);
